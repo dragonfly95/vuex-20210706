28163ec936937c82ee2ed8d8c03bb0cc5e30c0bf
dragonfly95 <nosignal95@gmail.com>
2022-02-11 12:34:20 +0900

    c

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,8 +1,26 @@
 <template>
-    <div class="panel panel-default">
-        <div class="panel-heading">Panels</div>
-        <div class="panel-body">
-            Panel content 
+
+
+    <div class="container">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="size in lista" v-bind:key="size.brnSz">
+            {{size.sourceAsMap.nncSz}}
+            <div class="btn-group pull-right" style="font-size: 12px; line-height: 1;">
+                <button type="button" class="btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                더보기<span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                <li><a href="#">삭제</a></li>
+                </ul>
+            </div>
+            </li>
+        </ul>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Panels</div>
+            <div class="panel-body">
+                Panel content 
+            </div>
         </div>
     </div>
 </template>
@@ -16,7 +34,8 @@ export default {
     name: 'Example',
     data() {
         return {
-            msg: 'basic panel example'
+            msg: 'basic panel example',
+            lista: [],
         }
     },
     created() {
@@ -24,7 +43,11 @@ export default {
     },
     methods: {
         test() {
-            instance.get('/v1/api/fitsize');
+            instance.get('/v1/api/fitsize')
+            .then(response => {
+                this.lista = response.data;
+                console.log(this.lista);
+            });
         }
     }
 }
